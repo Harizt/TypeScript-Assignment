b5d5b7f8cc9c2cd9655822bb0bb63a48cd8eee60
Haris <tanatip@nsquared.asia>
2023-07-03 16:37:53 +0700

    update test case

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -16,4 +16,7 @@ describe('testing index file', () => {
   test('case 5: shuffle and duplicate value in collection 1 and collection 2', () => {
     expect(mergeSortArray([1,1,7,8,10], [3,3,9,9,12])).toStrictEqual([1, 1, 3, 3, 7, 8, 9, 9, 10, 12]);
   });
+  test('case 6: shuffle, duplicate and array length are not equal between collection 1 and collection 2.', () => {
+    expect(mergeSortArray([1,1,7,8,10,20,33,66,99], [3,3,9,9,12,19])).toStrictEqual([1, 1, 3, 3, 7, 8, 9, 9, 10, 12, 19, 20, 33, 66, 99]);
+  });
 });
